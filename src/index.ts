a7492c8b58c6d86bcd88bc7bf5a58a9c825d2ecd
fizban007 <fizban007@gmail.com>
2020-05-27 10:12:43 -0400

    included another way of handling the frame transition

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,9 @@ import "@babylonjs/core/Loading/loadingScreen";
 import { AssetsManager, BinaryFileAssetTask } from "@babylonjs/core/Misc/assetsManager";
 import { LinesMesh } from "@babylonjs/core/Meshes/linesMesh";
 
-// import * as stats from "stats.js";
 import Stats from "stats.js";
+import { int } from "@babylonjs/core/types";
+import { KeyboardEventTypes } from "@babylonjs/core/Events/keyboardEvents";
 // import Stats = require("stats.js");
 
 // Get the canvas element from the DOM.
@@ -67,54 +68,166 @@ star.material = material;
 var assetsManager = new AssetsManager(scene);
 assetsManager.useDefaultLoadingScreen = true;
 
-for (var i = 0; i < 6; i++) {
-    assetsManager.addBinaryFileTask("loadLine" + i, "data/line_" + i + "_000000");
+var lines: LinesMesh[][] = [];
+var line_meshes: LinesMesh;
+var buffers: Vector3[][][] = [];
+
+var n_init: int = 100;
+var n_max: int = 400;
+var n_current: int = n_init;
+var n_prev: int = n_current;
+var is_playing: boolean = false;
+
+for (var n = 0; n <= n_max - n_init; n++) {
+    lines.push([]);
+    buffers.push([]);
 }
 
-var lines : LinesMesh[] = [];
+function create_line_buffer(j: int, n: int, lines: LinesMesh[][]) {
+    return function(task: BinaryFileAssetTask) {
+        var floatView = new Float32Array(task.data);
+        var vertices: Vector3[] = [];
+        for (var i = 0; i < floatView.length / 3; i++) {
+            vertices.push(new Vector3(floatView[i * 3 + 1],
+                floatView[i * 3 + 2],
+                floatView[i * 3]));
+        }
+        buffers[j - n_init].push(vertices);
+    }
+}
 
-assetsManager.load();
-assetsManager.onFinish = function(tasks) {
-    tasks.forEach( (element) => {
-        // console.log(task.data);
-        var task = element as BinaryFileAssetTask;
-        var floatView = new Float32Array(task!.data);
+function update_lines(n: int) {
+    scene.removeMesh(line_meshes);
+    line_meshes = LinesBuilder.CreateLineSystem("fieldlines", {
+        lines: buffers[n - n_init],
+        useVertexAlpha: false,
+    }, scene);
+}
+
+function create_lines(j: int, n: int, lines: LinesMesh[][]) {
+    return function(task: BinaryFileAssetTask) {
+        var floatView = new Float32Array(task.data);
         var vertices: Vector3[] = [];
         var colors: Color4[] = [];
         for (var i = 0; i < floatView.length / 3; i++) {
             vertices.push(new Vector3(floatView[i * 3 + 1],
-                                      floatView[i * 3 + 2],
-                                      floatView[i * 3]));
+                floatView[i * 3 + 2],
+                floatView[i * 3]));
             colors.push(new Color4(0, 1, 0, 1));
         }
-        lines.push(LinesBuilder.CreateLines("fieldline0", {
+        // console.log(n, j, "loading finished");
+        var l = LinesBuilder.CreateLines("fieldline_" + j + "_" + n, {
             points: vertices,
             useVertexAlpha: false,
             colors: colors,
-        }, scene));
-
-    });
-}
-var gl = new GlowLayer("glow", scene);
-gl.intensity = 1.5;
-gl.customEmissiveColorSelector = (mesh, subMesh, material, result) => {
-    lines.forEach( (line) => {
-        if (mesh == line) {
-            result.set(0, 1, 0, 0.5);
+        }, scene);
+        if (j != n_init) {
+            l.setEnabled(false);
         }
-    } );
-    if (mesh == star) {
-        result.set(0, 0, 0, 1);
+        lines[j - n_init].push(l);
     }
 }
 
-var st = new Stats();
-st.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(st.dom);
+function enable_lines(n: int, n_prev: int) {
+    lines[n_prev - n_init].forEach((line) => {
+        line.setEnabled(false);
+    });
+    lines[n - n_init].forEach((line) => {
+        line.setEnabled(true);
+    });
+}
 
-// Render every frame
-engine.runRenderLoop(() => {
-    st.begin();
-    scene.render();
-    st.end();
-});
+for (var n = 0; n < 6; n++) {
+    for (var j = n_init; j <= n_max; j++) {
+        var linetask = assetsManager.addBinaryFileTask("loadLine" + n + "_" + (j * 500),
+            "data/line_" + n + "_" + ("000000" + j * 500).slice(-6));
+        // linetask.onSuccess = create_lines(j, n, lines);
+        linetask.onSuccess = create_line_buffer(j, n, lines);
+    }
+}
+// function create_load_task(n: int, num_line: int) {
+//     var linetask = assetsManager.addBinaryFileTask("loadLine" + num_line + "_" + (n * 500),
+//         "data/line_" + num_line + "_" + ("000000" + n * 500).slice(-6));
+//     linetask.onSuccess = function() {
+//         console.log("Finished loading ", n, num_line);
+//         create_lines(n, num_line, lines);
+//         if (n < n_max) {
+//             create_load_task(n + 1, num_line);
+//         }
+//     }
+// }
+
+// for (var n = 0; n < 6; n++) {
+//     create_load_task(n_init, n);
+// }
+
+function show_next_frame() {
+    n_prev = n_current;
+    n_current += 1;
+    if (n_current > n_max) n_current = n_init;
+    // enable_lines(n_current, n_prev);
+    update_lines(n_current);
+}
+
+function show_prev_frame() {
+    n_prev = n_current;
+    n_current -= 1;
+    if (n_current < n_init) n_current = n_max;
+    // enable_lines(n_current, n_prev);
+    update_lines(n_current);
+}
+
+assetsManager.onFinish = function(tasks) {
+    update_lines(n_init);
+    var gl = new GlowLayer("glow", scene);
+    gl.intensity = 1.5;
+    gl.customEmissiveColorSelector = (mesh, subMesh, material, result) => {
+        // lines.forEach((line_set) => {
+        //     line_set.forEach((line) => {
+        //         if (mesh == line) {
+        //             result.set(0, 1, 0, 0.5);
+        //         }
+        //     });
+        // });
+        if (mesh == line_meshes)
+            result.set(0, 1, 0, 0.5);
+        if (mesh == star)
+            result.set(0, 0, 0, 1);
+    }
+
+    var st = new Stats();
+    st.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild(st.dom);
+
+    // Add keypress events
+    scene.onKeyboardObservable.add((kbInfo) => {
+        if (kbInfo.type == KeyboardEventTypes.KEYUP) {
+            if (kbInfo.event.key == "ArrowRight") {
+                show_next_frame();
+            } else if (kbInfo.event.key == "ArrowLeft") {
+                show_prev_frame();
+            } else if (kbInfo.event.key == " ") {
+                is_playing = !is_playing;
+            }
+        }
+    });
+
+    // Render every frame
+    var startTime = Date.now();
+
+    engine.runRenderLoop(() => {
+        st.begin();
+        if (is_playing) {
+            var currentTime = Date.now();
+            var timeDiff = currentTime - startTime;
+            if (timeDiff > 40) {
+                show_next_frame();
+                startTime = currentTime;
+            }
+        }
+        scene.render();
+        st.end();
+    });
+}
+// console.log(assetsManager.)
+assetsManager.loadAsync();
