95eee6fe7b2240e898e10660ba128f47fc74ff33
fizban007 <fizban007@gmail.com>
2020-06-25 15:42:13 -0400

    added compression to json

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import app
-from flask import render_template, send_from_directory, send_file
+from flask import render_template, send_from_directory, send_file, request
 import hashlib
 import io
 import sys
+import gzip
+import json
 sys.path.append("/home/alex/Projects/CoffeeGPU/python/")
 # sys.path.append("/home/alex/Projects/Aperture4/python/")
 from datalib import Data
@@ -15,7 +17,11 @@ def load_sph_data(data_path):
     hex_dig = hashlib.md5(data_path.encode()).hexdigest()
     data = Data(data_path)
     # print(hex_dig)
-    return data._conf
+    content = gzip.compress(json.dumps(data._conf).encode('utf8'), 5)
+    response = make_response(content)
+    response.headers['Content-length'] = len(content)
+    response.headers['Content-Encoding'] = 'gzip'
+    return response
 
 @app.route('/')
 @app.route('/index')
@@ -34,3 +40,11 @@ def get_fieldlines(step):
     # If data is not loaded then don't do anything
     if data is None:
         return None
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        player_data = request.json["data"]
+        return player_data
+    else:
+        return "Get method!"
