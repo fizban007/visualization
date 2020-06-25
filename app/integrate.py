cdd03e1f35bf45373575511eae78e2490bb310da
fizban007 <fizban007@gmail.com>
2020-06-25 17:06:09 -0400

    added field line integration

---
+++ app/integrate.py
@@ -0,0 +1,73 @@
+from app.rk_integrate import Euler_integrate
+import numpy as np
+import math
+
+class Bf:
+    def __init__(self, data):
+        B = np.sqrt(data.Bx * data.Bx + data.By * data.By + data.Bz * data.Bz)
+        self.Bx = data.Bx / B
+        self.By = data.By / B
+        self.Bz = data.Bz / B
+        self.x0 = data._conf['lower'][0]
+        self.y0 = data._conf['lower'][1]
+        self.z0 = data._conf['lower'][2]
+        self.dx = data._conf['size'][0] / data._conf['N'][0] * data._conf['downsample']
+        self.dy = data._conf['size'][1] / data._conf['N'][1] * data._conf['downsample']
+        self.dz = data._conf['size'][2] / data._conf['N'][2] * data._conf['downsample']
+
+    def interp(self, f, x, y, z):
+        nx = int(math.floor((x - self.x0) / self.dx))
+        ny = int(math.floor((y - self.y0) / self.dy))
+        nz = int(math.floor((z - self.z0) / self.dz))
+        delx = x - self.x0 - nx * self.dx
+        dely = y - self.y0 - ny * self.dy
+        delz = z - self.z0 - nz * self.dz
+    #     print(delx, dely, delz)
+        f00 = (1.0 - delz) * f[nz,ny,nx] + delz * f[nz+1,ny,nx]
+        f01 = (1.0 - delz) * f[nz,ny,nx+1] + delz * f[nz+1,ny,nx+1]
+        f10 = (1.0 - delz) * f[nz,ny+1,nx] + delz * f[nz+1,ny+1,nx]
+        f11 = (1.0 - delz) * f[nz,ny+1,nx+1] + delz * f[nz+1,ny+1,nx+1]
+        f0 = (1.0 - dely) * f00 + dely * f10
+        f1 = (1.0 - dely) * f01 + dely * f11
+        return (1.0 - delx) * f0 + delx * f1
+
+    def value(self, x, y):
+        B = np.array([self.interp(self.Bx, y[0], y[1], y[2]),
+                      self.interp(self.By, y[0], y[1], y[2]),
+                      self.interp(self.Bz, y[0], y[1], y[2])])
+        return B
+
+    def value_neg(self, x, y):
+        return -self.value(x, y)
+
+
+def integrate_fields_sphere(r_seed, n_samples, data):
+    data_b = Bf(data)
+
+    p_seed = []
+    for n in range(n_samples):
+        mu = np.random.random_sample() * 2.0 - 1.0
+        th = np.arccos(mu)
+        ph = 2*np.pi*np.random.random_sample()
+        p = r_seed * np.array([np.sin(th) * np.sin(ph),
+                               np.sin(th) * np.cos(ph),
+                               np.cos(th)])
+        p_seed.append(p)
+    box_size = abs(max(data._conf['lower']))
+
+    def end_box(x, y):
+        r = np.sqrt(sum(y*y))
+        dist = np.max(abs(y))
+        return r < 1.0 or dist > box_size
+
+    lines = []
+    for p in p_seed:
+        xs, ys = Euler_integrate(0.0, p, 0.05, data_b.value, end_box, 1000)
+        xs2, ys2 = Euler_integrate(0.0, p, 0.05, data_b.value_neg, end_box, 1000)
+        if len(ys) > 100:
+            lines.append(np.concatenate((ys, ys2[::-1]))[::10])
+        elif len(ys) > 20:
+            lines.append(np.concatenate((ys, ys2[::-1]))[::4])
+        else:
+            lines.append(np.concatenate((ys, ys2[::-1])))
+    return lines
--- src/fieldlines.ts
+++ src/fieldlines.ts
@@ -1,12 +1,233 @@
+import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
-import { Nullable } from "@babylonjs/core/types";
-import { LinesMesh } from "@babylonjs/core/Meshes/linesMesh";
+import { Vector3, Color3, Color4 } from "@babylonjs/core/Maths/math";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
+
+import { SphereBuilder } from "@babylonjs/core/Meshes/Builders/sphereBuilder";
 import { LinesBuilder } from "@babylonjs/core/Meshes/Builders/linesBuilder";
+import { GlowLayer } from "@babylonjs/core/Layers/glowLayer";
 
-export declare class FieldlineBuilder {
+// Load LoadingScreen for side effects
+import "@babylonjs/core/Loading/loadingScreen";
+import { AssetsManager, BinaryFileAssetTask } from "@babylonjs/core/Misc/assetsManager";
+import { LinesMesh } from "@babylonjs/core/Meshes/linesMesh";
 
-    static CreateFieldlines(name: string, options: {
+import Stats from "stats.js";
+import { int } from "@babylonjs/core/types";
+import { KeyboardEventTypes } from "@babylonjs/core/Events/keyboardEvents";
+// import Stats = require("stats.js");
 
-    }, scene: Nullable<Scene>): LinesMesh;
+// Get the canvas element from the DOM.
+const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
+// Associate a Babylon Engine to it.
+const engine = new Engine(canvas);
+
+// Create our first scene.
+var scene = new Scene(engine);
+
+scene.clearColor = new Color4(0, 0, 0, 1);
+
+// This creates and positions a free camera (non-mesh)
+var camera = new ArcRotateCamera("camera1", 0, 0, 10, new Vector3(0, 0, 0), scene);
+
+// This targets the camera to scene origin
+// camera.setTarget(Vector3.Zero());
+camera.setPosition(new Vector3(20, 0, 0));
+
+// This attaches the camera to the canvas
+camera.attachControl(canvas, true);
+
+// camera.allowUpsideDown = false;
+camera.lowerRadiusLimit = 1;
+camera.upperRadiusLimit = 100;
+camera.wheelPrecision = 10.0;
+
+// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+
+// Default intensity is 1. Let's dim the light a small amount
+light.intensity = 0.7;
+
+// Create a grid material
+// var material = new GridMaterial("grid", scene);
+var material = new StandardMaterial("mat", scene);
+material.backFaceCulling = false;
+
+// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+// var sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
+var star = SphereBuilder.CreateSphere("star",
+    { diameter: 2 }, scene);
+
+// Affect a material
+star.material = material;
+
+// Create an asset manager and load field line
+var assetsManager = new AssetsManager(scene);
+assetsManager.useDefaultLoadingScreen = true;
+
+var lines: LinesMesh[][] = [];
+var line_meshes: LinesMesh;
+var buffers: Vector3[][][] = [];
+
+var n_init: int = 100;
+var n_max: int = 400;
+var n_current: int = n_init;
+var n_prev: int = n_current;
+var is_playing: boolean = false;
+
+for (var n = 0; n <= n_max - n_init; n++) {
+    lines.push([]);
+    buffers.push([]);
 }
+
+function create_line_buffer(j: int, n: int, lines: LinesMesh[][]) {
+    return function(task: BinaryFileAssetTask) {
+        var floatView = new Float32Array(task.data);
+        var vertices: Vector3[] = [];
+        for (var i = 0; i < floatView.length / 3; i++) {
+            vertices.push(new Vector3(floatView[i * 3 + 1],
+                floatView[i * 3 + 2],
+                floatView[i * 3]));
+        }
+        buffers[j - n_init].push(vertices);
+    }
+}
+
+function update_lines(n: int) {
+    scene.removeMesh(line_meshes);
+    line_meshes = LinesBuilder.CreateLineSystem("fieldlines", {
+        lines: buffers[n - n_init],
+        useVertexAlpha: false,
+    }, scene);
+}
+
+function create_lines(j: int, n: int, lines: LinesMesh[][]) {
+    return function(task: BinaryFileAssetTask) {
+        var floatView = new Float32Array(task.data);
+        var vertices: Vector3[] = [];
+        var colors: Color4[] = [];
+        for (var i = 0; i < floatView.length / 3; i++) {
+            vertices.push(new Vector3(floatView[i * 3 + 1],
+                floatView[i * 3 + 2],
+                floatView[i * 3]));
+            colors.push(new Color4(0, 1, 0, 1));
+        }
+        // console.log(n, j, "loading finished");
+        var l = LinesBuilder.CreateLines("fieldline_" + j + "_" + n, {
+            points: vertices,
+            useVertexAlpha: false,
+            colors: colors,
+        }, scene);
+        if (j != n_init) {
+            l.setEnabled(false);
+        }
+        lines[j - n_init].push(l);
+    }
+}
+
+function enable_lines(n: int, n_prev: int) {
+    lines[n_prev - n_init].forEach((line) => {
+        line.setEnabled(false);
+    });
+    lines[n - n_init].forEach((line) => {
+        line.setEnabled(true);
+    });
+}
+
+for (var n = 0; n < 6; n++) {
+    for (var j = n_init; j <= n_max; j++) {
+        var linetask = assetsManager.addBinaryFileTask("loadLine" + n + "_" + (j * 500),
+            "data/line_" + n + "_" + ("000000" + j * 500).slice(-6));
+        // linetask.onSuccess = create_lines(j, n, lines);
+        linetask.onSuccess = create_line_buffer(j, n, lines);
+    }
+}
+// function create_load_task(n: int, num_line: int) {
+//     var linetask = assetsManager.addBinaryFileTask("loadLine" + num_line + "_" + (n * 500),
+//         "data/line_" + num_line + "_" + ("000000" + n * 500).slice(-6));
+//     linetask.onSuccess = function() {
+//         console.log("Finished loading ", n, num_line);
+//         create_lines(n, num_line, lines);
+//         if (n < n_max) {
+//             create_load_task(n + 1, num_line);
+//         }
+//     }
+// }
+
+// for (var n = 0; n < 6; n++) {
+//     create_load_task(n_init, n);
+// }
+
+function show_next_frame() {
+    n_prev = n_current;
+    n_current += 1;
+    if (n_current > n_max) n_current = n_init;
+    // enable_lines(n_current, n_prev);
+    update_lines(n_current);
+}
+
+function show_prev_frame() {
+    n_prev = n_current;
+    n_current -= 1;
+    if (n_current < n_init) n_current = n_max;
+    // enable_lines(n_current, n_prev);
+    update_lines(n_current);
+}
+
+assetsManager.onFinish = function(tasks) {
+    update_lines(n_init);
+    var gl = new GlowLayer("glow", scene);
+    gl.intensity = 1.5;
+    gl.customEmissiveColorSelector = (mesh, subMesh, material, result) => {
+        // lines.forEach((line_set) => {
+        //     line_set.forEach((line) => {
+        //         if (mesh == line) {
+        //             result.set(0, 1, 0, 0.5);
+        //         }
+        //     });
+        // });
+        if (mesh == line_meshes)
+            result.set(0, 1, 0, 0.5);
+        if (mesh == star)
+            result.set(0, 0, 0, 1);
+    }
+
+    var st = new Stats();
+    st.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild(st.dom);
+
+    // Add keypress events
+    scene.onKeyboardObservable.add((kbInfo) => {
+        if (kbInfo.type == KeyboardEventTypes.KEYUP) {
+            if (kbInfo.event.key == "ArrowRight") {
+                show_next_frame();
+            } else if (kbInfo.event.key == "ArrowLeft") {
+                show_prev_frame();
+            } else if (kbInfo.event.key == " ") {
+                is_playing = !is_playing;
+            }
+        }
+    });
+
+    // Render every frame
+    var startTime = Date.now();
+
+    engine.runRenderLoop(() => {
+        st.begin();
+        if (is_playing) {
+            var currentTime = Date.now();
+            var timeDiff = currentTime - startTime;
+            if (timeDiff > 40) {
+                show_next_frame();
+                startTime = currentTime;
+            }
+        }
+        scene.render();
+        st.end();
+    });
+}
+// console.log(assetsManager.)
+assetsManager.loadAsync();
